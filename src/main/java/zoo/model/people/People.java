e17867faa1c52f9d3f7e869523ffbbfae11420c9
mmiguelf <marwin-m.falqueza@gcash.com>
2025-07-11 13:23:58 +0800

    defined People class

--- src/main/java/zoo/model/people/People.java
+++ src/main/java/zoo/model/people/People.java
@@ -1,4 +1,12 @@
 package zoo.model.people;
 
+import zoo.model.building.Building;
+
 public abstract class People {
+    protected String name;
+    protected Building location;
+
+    public void goTo(Building newLocation){
+        this.location = newLocation;
+    };
 }
